3a78443e8e3815bc6514206ff4304e4e07a1ddaf
Eric Johns <eric275j@gmail.com>
2021-02-19 00:28:16 -0500

    Started some interface stuff
    infinite agony

--- main.py
+++ main.py
@@ -28,13 +28,59 @@ def play_sound():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app = tkinter.Tk()
-    app.geometry("500x200")
+    app.title("Music Notation")
+    app.geometry("1280x720")
+    app.resizable(False, False)
 
-    button = tkinter.Button(app, height=3, width=12, text="Play", command=play_sound)
-    button.pack()
+    app.rowconfigure(0, minsize=50, weight=1)
+    app.columnconfigure([0, 1, 2, 3, 4, 5, 6], minsize=50, weight=1)
 
-    button2 = tkinter.Button(app, height=3, width=12, text="Exit", command=quit)
-    button2.pack()
+    # Time Signatures --------------------------------------------------------------------------------------------------
+
+    timesig_buttons = ["4/4", "3/4", "5/4", "2/4", "12/8", "6/8"]
+
+    app.rowconfigure(1, minsize=50, weight=1)
+    app.columnconfigure([0, 1, 2, 3, 4, 5, 6], minsize=50, weight=1)
+
+    lbl_timesig = tkinter.Label(master=app, text="Time Signatures:", width=80, height=4)
+    lbl_timesig.config(font=("Arial", 24))
+    lbl_timesig.grid(row=1, column=0, padx=5)
+
+    for i in range(len(timesig_buttons)):
+        btn = tkinter.Button(master=app, text=timesig_buttons[i], width=200, height=4)
+        btn.grid(row=1, column=i+1, padx=5)
+
+    # Note Lengths -----------------------------------------------------------------------------------------------------
+
+    note_length_buttons = ["1/16", "1/8", "1/4", "1/2", "1"]
+
+    app.rowconfigure(2, minsize=50, weight=2)
+    app.columnconfigure([0, 1, 2, 3, 4], minsize=50, weight=1)
+
+    lbl_note = tkinter.Label(master=app, text="Note Lengths:", width=80, height=4)
+    lbl_note.config(font=("Arial", 24))
+    lbl_note.grid(row=2, column=0, padx=5)
+
+    for i in range(len(note_length_buttons)):
+        btn = tkinter.Button(master=app, text=note_length_buttons[i], width=200, height=4)
+        btn.grid(row=2, column=i+1, padx=5)
+
+    # Key Signatures ---------------------------------------------------------------------------------------------------
+
+    keysig_buttons = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
+
+    app.rowconfigure(3, minsize=50, weight=3)
+    app.columnconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], minsize=50, weight=1)
+
+    lbl_note = tkinter.Label(master=app, text="Key Signatures:", width=80, height=4)
+    lbl_note.config(font=("Arial", 20))
+    lbl_note.grid(row=3, column=0, padx=5)
+
+    for i in range(len(keysig_buttons)):
+        btn = tkinter.Button(master=app, text=keysig_buttons[i], width=200, height=4)
+        btn.grid(row=3, column=i+1, padx=5)
+
+    # -----------------------------------------------------------------------------------------------------------------
 
     app.mainloop()
 
